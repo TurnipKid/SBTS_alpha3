12ece54ff737a73be355ed244875efb34c408a44
Katie <katie.m.frendo@gmail.com>
2025-11-16 23:10:33 +0100

    ACCESSORY LOGIC MODIFICATIONS
    plus some minor fitting tweaks

--- Assets/draglogic.cs
+++ Assets/draglogic.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f2b5553532c430e1369f34b3ec784d86bc18a76ea0a293a077eed95b73ed900
-size 11754
+oid sha256:6445549ffb68c7726ba75af43b41ce34f5a3e906a16e2e74388fcb6cdf508d7b
+size 12758
